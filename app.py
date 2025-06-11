87847ec287f0bf11340425ce63b484a9167ecff7
tirthpatel143 <tirth.p.patel143@gmail.com>
2025-06-11 21:35:22 +0530

    QR code generator

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    url = request.form['url']
+    qr_img = qrcode.make(url)
+    
+    img_io = io.BytesIO()
+    qr_img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
